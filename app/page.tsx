ea8a1aad4c6c0e7ebdd78f22a7ff60c144c4a693
Fırat Altun <altunnfirat@gmail.com>
2025-02-26 01:31:24 +0300

    code refactor

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-import ModelViewer from "@/components/model-viewer";
+
 
 export default function Page() {
      <div className="w-screen h-screen bg-[#CEEFFD] relative">
-      <ModelViewer />
+     
     </div>
 }
